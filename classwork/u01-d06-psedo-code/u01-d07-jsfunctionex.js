ff26afd1b1dac3dd81eb6b9216509093185e7c86
naeohmi <naeohmi@gmail.com>
2017-04-04 16:04:24 -0400

    constructor functions

--- classwork/u01-d06-psedo-code/u01-d07-jsfunctionex.js
+++ classwork/u01-d06-psedo-code/u01-d07-jsfunctionex.js
@@ -1,3 +1,5 @@
+//#FUNCTION / OBJECT FUN
+
 // let arr = [1, true, 'hello', function () { return('Hi!')}];
 
 // console.log(arr[3]());
@@ -21,9 +23,67 @@ console.log(add3(2, 3));
 //could add default parameters for values you want defaulted
 // let add3 = (n1=1, n2=20) => n1 + n2;
 
-let bazz = (function() {
-	return {
-		foo: 123
-	};
-})();
-console.log(bazz);
+//IFFE way:
+// let bazz = (function() {
+// 	return {
+// 		foo: 123
+// 	};
+// })();
+// console.log(bazz);
+
+//#THIS FUN
+
+var name = 'bug';
+let car = {
+	insurance: 'expensive',
+	model: 2017,
+	company: 'toyota',
+	color: 'red',
+	used: false,
+	name: this.name
+}
+
+// console.log(car.name);
+
+function getName() {
+	console.log(this.name);
+}
+var obj = {
+	name: 'Naomi'
+}
+var obj1 = {
+	name: 'Ella'
+}
+var obj2 = {
+	name: 'Patrick'
+}
+getName.apply(obj2);
+
+
+function Car(color, engine, year) {
+	this.color = color;
+	this.engine = engine
+	this.year = year;
+}
+let lambo = new Car('Silver', 'v8', 1997);
+let mazarti = new Car('Gold', 'f16', 2017);
+
+// console.log(lambo.color, lambo.engine, lambo.year);
+// console.log(mazarti.color, mazarti.engine, mazarti.year);
+
+//#CLASSES!
+
+class Car2 { //mechanism to hold all the logic
+	constructor(engine) { //factory - everytime you create a class, it always needs a constructor
+		this.engine = engine;
+		this.idle = true;
+		console.log('I exist', this.engine);
+	}
+
+	getEngine() {
+		console.log(this.engine);
+	}
+}
+let bugatti = new Car2('v12');
+// let tesla = new Car2('electric');
+bugatti.getEngine();
